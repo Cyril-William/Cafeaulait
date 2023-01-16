6d73f583a2ab980459edbbcfe25f99d09090fc2a
WILLIAM SOUEF <williamsouef@gmail.com>
2023-01-16 18:04:44 +0100

    Initial Commit

---
+++ Cafeaulait/CafeaulaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeaulaitApp.swift
+//  Cafeaulait
+//
+//  Created by William Souef on 16/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CafeaulaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
